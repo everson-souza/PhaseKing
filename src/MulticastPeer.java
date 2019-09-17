bcd94b07343523dc681373747312c4470293ae99
Everson <everson@alunos.utfpr.edu.br>
2019-09-17 15:30:33 -0300

    TERMINA VAGABUNDA

--- src/MulticastPeer.java
+++ src/MulticastPeer.java
@@ -4,6 +4,20 @@ import java.util.*;
 import java.util.Scanner;
 import org.json.*;
 public class MulticastPeer{
+    static int idProcesso, valor, phase = 0;
+    static int mult, majority = -1;
+    public static int getIdProcesso(){
+        return MulticastPeer.idProcesso;
+    }
+    
+    public static void setIdProcesso(int idProcesso) {
+        MulticastPeer.idProcesso = idProcesso;
+    }
+    
+    public static void setDatagrama(int mult, int majority) {
+        MulticastPeer.mult = mult;
+        MulticastPeer.majority = majority;
+    }
     
     public static void main(String args[]){ 
         // args give message contents and destination multicast group (e.g. "228.5.6.7")
@@ -14,37 +28,33 @@ public class MulticastPeer{
                 s = new MulticastSocket(6789);
                 s.joinGroup(group); 		
                 //Lê do teclado o id do processo e a mensagem                
-                int idProcesso, valor, phase = 0;
-                
+                                  
                 Scanner leID = new Scanner(System.in);               
                 System.out.println("Digite o ID do processo:");
-                idProcesso = leID.nextInt();
+                
+                setIdProcesso(leID.nextInt());
                 
                 Scanner leValor = new Scanner(System.in);
                 System.out.println("Digite a mensagem:");
                 valor = leValor.nextByte();
                 
-                JSONObject message = new JSONObject();
-                message.put("i", idProcesso);
-                message.put("v", valor);
-                
-                //for (phase = 0; phase < 2; phase++ ){
+                //for (int i=0; i< 2; i++){
+                    JSONObject message = new JSONObject();
+                    message.put("i", getIdProcesso());
+                    message.put("v", valor);                    
+
                     String msg = message.toString();
 
                     //Converte para um byte array e envia
                     byte [] m = msg.getBytes();                        
-                    DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
-                    s.send(messageOut);			
-                    //Cria a thread da classe Connection e passa o socket por parâmetro
-                    Connection c = new Connection(s);
-
-                    if (idProcesso == phase){
-                        //Converte para um byte array e envia
-                        byte [] byteMajority = majority.getBytes();                        
-                        DatagramPacket sendMajority = new DatagramPacket(byteMajority, byteMajority.length, group, 6789);
-                        s.send(sendMajority);
-                    }
-                //}
+                    DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);               
+                    
+                    s.send(messageOut);	
+                //\\\\\\\\\\\\\}		
+                //Cria a thread da classe Connection e passa o socket por parâmetro
+                Connection c = new Connection(s);
+                
+                
         }catch (SocketException e){
             System.out.println("Socket: " + e.getMessage());
         }catch (IOException e){
@@ -56,51 +66,76 @@ public class MulticastPeer{
 }
 
 class Connection extends Thread {
-	DataInputStream in;
-	DataOutputStream out;
-	MulticastSocket clientSocket;
-       // Vetor de respostas
-        int vetor[] = new int[5];
-        int processos = 5;
-        int zero, um, mult, majority;
-	
-        public Connection (MulticastSocket aClientSocket) {
-		try {
-			clientSocket = aClientSocket;
-			byte[] buffer = new byte[1000];
-                        int i, v;
- 			for(int j=0; j< 6;j++) {		// get messages from others in group
-                            DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
-                            clientSocket.receive(messageIn);
-                            JSONObject msg_recebida = new JSONObject(new String(messageIn.getData())); 				
-                            i = msg_recebida.getInt("i");
-                            v = msg_recebida.getInt("v");
-                            System.out.println("Id: " + i+ " Valor: "+v);                                
-                            vetor[j] = v; 
-                            
-                            //Verifica majority e maioria
-                            if (j == 4){
-                                for(int k=0; k<vetor.length;k++) {
-                                    if (vetor[k] == 0) 
-                                        zero++;
-                                    if (vetor[k]==1)
-                                        um++;
-                                }
-                                if (zero>um){
-                                    mult = 0;
-                                    majority = zero;
-                                }else if (zero < um){
-                                    mult = 1;
-                                    majority = um; 
-                                }else{
-                                    mult = -1; //Empate
-                                    majority = -1;
-                                }
-                                System.out.println("Majority: "+majority+" Mult:"+mult );
-                            }
-                        }                        
-			this.start();
-                 } catch(IOException e) {System.out.println("Connection:"+e.getMessage());}
-	}
+    DataInputStream in;
+    DataOutputStream out;
+    MulticastSocket clientSocket;
+   // Vetor de respostas
+    int vetor[] = new int[5];
+    int processos = 5;
+    int zero, um, mult, majority;
+    MulticastPeer multicastPeer = new MulticastPeer(); 
+    int A = multicastPeer.getIdProcesso();
 
+    public Connection (MulticastSocket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            byte[] buffer = new byte[1000];
+            int i, v, p;
+            for(int j=0; j< 6;j++) {		// get messages from others in group
+
+                DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+
+                clientSocket.receive(messageIn);
+                JSONObject msg_recebida = new JSONObject(new String(messageIn.getData())); 				
+                i = msg_recebida.getInt("i");
+                v = msg_recebida.getInt("v");
+                
+                System.out.println("Id: " + i+ " Valor: "+v);                                
+                vetor[j] = v; 
+
+                //Verifica majority e maioria
+                if (j == 4){
+                    for(int k=0; k<vetor.length;k++) {
+                        if (vetor[k] == 0) 
+                            zero++;
+                        if (vetor[k]==1)
+                            um++;
+                    }
+                    if (zero>um){
+                        mult = 0;
+                        majority = zero;
+                    }else if (zero < um){
+                        mult = 1;
+                        majority = um; 
+                    }else{
+                        mult = -1; //Empate
+                        majority = -1;
+                    }
+                    System.out.println("Majority: "+majority+" Mult:"+mult+" ID: "+A );
+                    multicastPeer.setDatagrama(mult, majority);
+                }
+            }                        
+            this.start();
+         } catch(IOException e) {System.out.println("Connection:"+e.getMessage());}
+    }
+}
+class SegundoRound extends Thread {
+    DataInputStream in;
+    DataOutputStream out;
+    MulticastSocket clientSocket;
+   // Vetor de respostas
+    int vetor[] = new int[5];
+    int processos = 5;
+    int zero, um, mult, majority;
+    MulticastPeer multicastPeer = new MulticastPeer(); 
+    int A = multicastPeer.getIdProcesso();
+
+    public SegundoRound (MulticastSocket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+                                             
+            this.start();
+         } 
+        catch(Exception e) {System.out.println("Connection:"+e.getMessage());}
+    }
 }
